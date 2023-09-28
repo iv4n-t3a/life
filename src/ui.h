6c2685456a7a03a6649b1436e3b4f04852eae20a
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2023-09-28 22:27:09 +0300

    Refactore ui implementation with state pattern

--- src/ui.h
+++ src/ui.h
@@ -4,6 +4,7 @@
 #include <array>
 #include <string>
 #include <tuple>
+#include <memory>
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
@@ -12,22 +13,36 @@
 #include "life.h"
 
 
+class UIState;
+
 class UI {
+	friend UIState;
+
 	Life life;
 	sf::RenderWindow& window;
 	int win_w, win_h, win_x, win_y, square_size;
 	Config cfg;
+	UIState* state;
 public:
 	UI(sf::RenderWindow&, Life, Config);
-	void setup();
 	void run();
 private:
-	void switch_clicked(sf::Event mouseclick);
+	void process_click(sf::Event click);
 	void move_window(sf::Event keypress);
 	void update_scale(sf::Event wheelscroll);
 	void redraw();
+
+	void switch_state(UIState* next);
 	sf::Color to_sf_color(Color);
 };
 
+class UIState {
+public:
+	virtual void update_life(Life&) = 0;
+	virtual void process_click(UI*, Life&, int x, int y) = 0;
+	virtual void process_space_pressed(UI*) = 0;
+protected:
+	void switch_state(UI*, UIState* next);
+};
 
 #endif // #ifndef UI_H
